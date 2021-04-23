df3fc0777d71bc6831abb74e1681c920dbe3cd79
LordGurr <50996232+LordGurr@users.noreply.github.com>
2021-04-23 11:11:36 +0200

    Created guild save data

--- DiscordBot/Bot.cs
+++ DiscordBot/Bot.cs
@@ -2986,6 +2986,32 @@ namespace DiscordBot
             }
         }
 
+        private class GuildSaveData
+        {
+            public bool finished = false;
+            public List<ChannelSaveData> kanalerIGuild = new List<ChannelSaveData>();
+            public ulong guild;
+            public DiscordGuild discordGuild;
+
+            public GuildSaveData(List<ChannelSaveData> _kanalerIGuild)
+            {
+                kanalerIGuild = _kanalerIGuild;
+                finished = true;
+            }
+
+            public GuildSaveData(DiscordGuild _discordGuild)
+            {
+                discordGuild = _discordGuild;
+                finished = false;
+            }
+
+            public GuildSaveData(ulong _guild)
+            {
+                guild = _guild;
+                finished = false;
+            }
+        }
+
         [Serializable]
         private class ChannelSaveData
         {
@@ -3284,43 +3310,6 @@ namespace DiscordBot
 
                 [DllImport("user32.dll")]
                 public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
-            }
-        }
-
-        public static class ScreenShotHelper
-        {
-            private static Bitmap CopyFromScreen(Rectangle bounds)
-            {
-                try
-                {
-                    var image = new Bitmap(bounds.Width, bounds.Height);
-                    using var graphics = Graphics.FromImage(image);
-                    graphics.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
-                    return image;
-                }
-                catch (Win32Exception)
-                {//When screen saver is active
-                    return null;
-                }
-            }
-
-            public static Image Take(Rectangle bounds)
-            {
-                return CopyFromScreen(bounds);
-            }
-
-            public static byte[] TakeAsByteArray(Rectangle bounds)
-            {
-                using var image = CopyFromScreen(bounds);
-                using var ms = new MemoryStream();
-                image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-                return ms.ToArray();
-            }
-
-            public static void TakeAndSave(string path, Rectangle bounds, System.Drawing.Imaging.ImageFormat imageFormat)
-            {
-                using var image = CopyFromScreen(bounds);
-                image.Save(path, imageFormat);
             }
         }
 
